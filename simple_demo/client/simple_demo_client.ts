718dc507456c92264561cac5dccc34b338c4ca1b
cd606 <64788159+cd606@users.noreply.github.com>
2020-05-11 15:28:49 -0500

    code reorganization

--- simple_demo/client/simple_demo_client.ts
+++ simple_demo/client/simple_demo_client.ts
@@ -1,14 +1,10 @@
-import {load, Type} from "protobufjs"
-import {connect} from "amqplib/callback_api"
-import {v4 as uuidv4} from "uuid"
-import {eddsa as EDDSA} from "elliptic"
+import {TMHelper} from "./tm_helpers"
+import { runInContext } from "vm";
 
 const signature_key_bytes = Buffer.from([
     0x89,0xD9,0xE6,0xED,0x17,0xD6,0x7B,0x30,0xE6,0x16,0xAC,0xB4,0xE6,0xD0,0xAD,0x47,
     0xD5,0x0C,0x6E,0x5F,0x11,0xDF,0xB1,0x9F,0xFE,0x4D,0x23,0x2A,0x0D,0x45,0x84,0x8E
 ]);
-const curve = new EDDSA("ed25519");
-const signature_key = curve.keyFromSecret(signature_key_bytes);
 
 let modeStr = process.argv[2];
 enum Mode {
@@ -29,133 +25,90 @@ switch (modeStr) {
 }
 let cmd = process.argv[3];
 
-function attachIdentityToBuffer(input : Uint8Array, mode : Mode) {
-    switch (mode) {
-        case Mode.Plain:
-            {
-                let inputBuffer = Buffer.from(input);
-                const myIdentity = "ts_client";
-                const identityLength = myIdentity.length;
-                let b = Buffer.alloc(8);
-                b.writeBigUInt64LE(BigInt(identityLength));
-                let ret = Buffer.concat([b, Buffer.from(myIdentity, 'ascii'), inputBuffer]);
-                return new Uint8Array(ret);
-            }
-            break;
-        case Mode.Sign:
-            {
-                let inputBuffer = Buffer.from(input);
-                let signature = signature_key.sign(inputBuffer);
-                let ret = Buffer.concat([inputBuffer,Buffer.from(signature.toBytes())]);
-                return new Uint8Array(ret);
-            }
-            break;
-        default:
-            return input;
-    }
+let tmHelper = new TMHelper();
+switch (mode) {
+    case Mode.Sign:
+        tmHelper.set_identity(signature_key_bytes);
+        break;
+    case Mode.Plain:
+        tmHelper.set_identity("simple_demo_client.ts");
+        break;
+    default:
+        break;
 }
 
-function sendBufferAndWaitForReply(queue : string, buffer : Uint8Array, replyType : Type) {
-    connect("amqp://guest:guest@localhost", function(error, connection) {
-        if (error) {
-            throw error;
-        }
-        connection.createChannel(function (error, channel) {
-            if (error) {
-                throw error;
-            }
-            channel.assertQueue('', {exclusive: true}, function (error, q) {
-                if (error) {
-                    throw error;
-                }
-                var correlationId = uuidv4();
-                channel.consume(q.queue, function(reply) {
-                    if (reply.properties.correlationId == correlationId) {
-                        let encoding = reply.properties.contentEncoding;
-                        if (encoding == 'with_final') {
-                            let finalFlag = (reply.content[reply.content.byteLength-1] != 0);
-                            let content = replyType.decode(reply.content.slice(0, reply.content.byteLength-1));
-                            console.log({
-                                content: content,
-                                finalFlag : finalFlag
-                            });
-                            if (finalFlag) {
-                                setTimeout(function() {
-                                    connection.close();
-                                    process.exit(0);
-                                }, 500);
-                            }
-                        } else {
-                            let content = replyType.decode(reply.content);
-                            console.log(content);
-                            setTimeout(function() {
-                                connection.close();
-                                process.exit(0);
-                            }, 500);
-                        }
-                    }
-                }, {noAck : true});
-                channel.sendToQueue(queue, Buffer.from(buffer), {
-                    correlationId : correlationId
-                    , replyTo: q.queue
-                });
-            });
-        });
-    });
+function callback(isFinal : boolean, data : {}) {
+    let obj = {isFinal : isFinal, data : data};
+    console.log(obj);
 }
-
-load("../proto/defs.proto", function(error, root) {
-    if (error) {
-        throw error;
+async function runConfigure(args : Array<string>) {
+    let enabled : boolean;
+    switch (args[0]) {
+    case 'enable':
+        enabled = true;
+        break;
+    case 'disable':
+        enabled = false;
+        break;
+    default:
+        console.log('Usage ... configure enable|disable');
+        process.exit();
     }
+    await tmHelper.set_incoming_and_outgoing_type(
+        '../proto/defs.proto'
+        , 'simple_demo.ConfigureResult'
+        , 'simple_demo.ConfigureCommand'
+    );
+    tmHelper.enable_identity();
+    await tmHelper.send_request(
+        'localhost::guest:guest:test_config_queue'
+        , {enabled: enabled}
+        , callback
+    );
+}
+async function runQuery(args: Array<string>) {
+    await tmHelper.set_incoming_and_outgoing_type(
+        '../proto/defs.proto'
+        , 'simple_demo.OutstandingCommandsResult'
+        , 'simple_demo.OutstandingCommandsQuery'
+    );
+    tmHelper.disable_identity();
+    await tmHelper.send_request(
+        'localhost::guest:guest:test_query_queue'
+        , {}
+        , callback
+    );
+}
+async function runClear(args: Array<string>) {
+    const ids = args.map((x) => parseInt(x));
+    await tmHelper.set_incoming_and_outgoing_type(
+        '../proto/defs.proto'
+        , 'simple_demo.ClearCommandsResult'
+        , 'simple_demo.ClearCommands'
+    );
+    tmHelper.enable_identity();
+    await tmHelper.send_request(
+        'localhost::guest:guest:test_clear_queue'
+        , {ids: ids}
+        , callback
+    );
+}
+async function runClient(args : Array<string>) {
     switch (cmd) {
     case 'configure':
-        {
-            let enabled : boolean;
-            switch (process.argv[4]) {
-            case 'enable':
-                enabled = true;
-                break;
-            case 'disable':
-                enabled = false;
-                break;
-            default:
-                console.log('Usage ... configure enable|disable');
-                process.exit();
-            }
-            const ConfigureCommand = root.lookupType("simple_demo.ConfigureCommand");
-            const ConfigureResult = root.lookupType("simple_demo.ConfigureResult");
-            let message = ConfigureCommand.create({
-                enabled : enabled
-            });
-            let buffer = attachIdentityToBuffer(ConfigureCommand.encode(message).finish(), mode);
-            sendBufferAndWaitForReply("test_config_queue", buffer, ConfigureResult);
-        }
+        await runConfigure(args);
         break;
     case 'query':
-        {
-            const OutstandingCommandsQuery = root.lookupType("simple_demo.OutstandingCommandsQuery");
-            const OutstandingCommandsResult = root.lookupType("simple_demo.OutstandingCommandsResult");
-            let message = OutstandingCommandsQuery.create({
-            });
-            //query does not require identity
-            let buffer = OutstandingCommandsQuery.encode(message).finish();
-            sendBufferAndWaitForReply("test_query_queue", buffer, OutstandingCommandsResult);
-        }
+        await runQuery(args);
         break;
     case 'clear':
-        {
-            const ids = process.argv.slice(4).map((x) => parseInt(x));
-            const ClearCommands = root.lookupType("simple_demo.ClearCommands");
-            const ClearCommandsResult = root.lookupType("simple_demo.ClearCommandsResult");
-            let message = ClearCommands.create({
-                ids: ids
-            });
-            let buffer = attachIdentityToBuffer(ClearCommands .encode(message).finish(), mode);
-            sendBufferAndWaitForReply("test_clear_queue", buffer, ClearCommandsResult);
-        }
+        await runClear(args);
         break;
     default:
         break;
     }
-});
+}
+
+(async () => {
+    await runClient(process.argv.slice(4));
+})();
---
+++ simple_demo/client/tm_helpers.ts
@@ -0,0 +1,200 @@
+import {load, Root, Message, Type} from "protobufjs"
+import * as amqp from 'amqplib'
+import {v4 as uuidv4} from "uuid"
+import {eddsa as EDDSA} from "elliptic"
+
+export class TMHelper {
+    private static parseLocator(locator : string) {
+        let idx = locator.indexOf('[');
+        let mainPortion : string;
+        let propertyPortion : string;
+        if (idx < 0) {
+            mainPortion = locator;
+            propertyPortion = "";
+        } else {
+            mainPortion = locator.substr(0, idx);
+            propertyPortion = locator.substr(idx);
+        }
+        let mainParts = mainPortion.split(':');
+        let properties = {};
+        if (propertyPortion.length > 2 && propertyPortion[propertyPortion.length-1] == ']') {
+            let realPropertyPortion = propertyPortion.substr(1, propertyPortion.length-2);
+            let propertyParts = realPropertyPortion.split(',');
+            for (let p of propertyParts) {
+                let nameAndValue = p.split('=');
+                if (nameAndValue.length == 2) {
+                    let name = nameAndValue[0];
+                    let value = nameAndValue[1];
+                    properties[name] = value;
+                }
+            }
+        }
+        let ret = {
+            "properties" : properties
+        };
+        if (mainParts.length >= 1) {
+            ret["host"] = mainParts[0];
+        } else {
+            ret["host"] = "";
+        }
+        if (mainParts.length >= 2 && mainParts[1] != "") {
+            ret["port"] = parseInt(mainParts[1]);
+        } else {
+            ret["port"] = 0;
+        }
+        if (mainParts.length >= 3) {
+            ret["username"] = mainParts[2];
+        } else {
+            ret["username"] = "";
+        }
+        if (mainParts.length >= 4) {
+            ret["password"] = mainParts[3];
+        } else {
+            ret["password"] = "";
+        }
+        if (mainParts.length >= 5) {
+            ret["identifier"] = mainParts[4];
+        } else {
+            ret["identifier"] = "";
+        }
+        return ret;
+    }
+
+    private has_identity : boolean;
+    private signature_key : EDDSA.KeyPair;
+    private plain_name : string;
+    private plain_name_length : number;
+    private use_signature : boolean;
+    private incoming_type : Type;
+    private outgoing_type : Type;
+
+    constructor() {
+        this.has_identity = false;
+    }
+
+    async set_incoming_type(protoFile: string, typeName : string) {
+        const protoRoot = await load(protoFile);
+        this.incoming_type = protoRoot.lookupType(typeName);
+    }
+    async set_outgoing_type(protoFile: string, typeName : string) {
+        const protoRoot = await load(protoFile);
+        this.outgoing_type = protoRoot.lookupType(typeName);
+    }
+    async set_incoming_and_outgoing_type(protoFile: string, incomingTypeName : string, outgoingTypeName : string) {
+        const protoRoot = await load(protoFile);
+        this.incoming_type = protoRoot.lookupType(incomingTypeName);
+        this.outgoing_type = protoRoot.lookupType(outgoingTypeName);
+    }
+
+    set_identity(identity: (Buffer|string)) {
+        if (Buffer.isBuffer(identity)) {
+            this.signature_key = new EDDSA("ed25519").keyFromSecret(identity);
+            this.use_signature = true;
+        } else {
+            this.plain_name = identity;
+            this.plain_name_length = this.plain_name.length;
+            this.use_signature = false;
+        }
+        this.has_identity = true;
+    }
+    disable_identity() {
+        this.has_identity = false;
+    }
+    enable_identity() {
+        this.has_identity = true;
+    }
+
+    attach_identity(input: Uint8Array) : Uint8Array {
+        if (!this.has_identity) {
+            return input;
+        }
+        if (this.use_signature) {
+            let inputBuffer = Buffer.from(input);
+            let signature = this.signature_key.sign(inputBuffer);
+            let ret = Buffer.concat([inputBuffer,Buffer.from(signature.toBytes())]);
+            return new Uint8Array(ret);
+        } else {
+            let inputBuffer = Buffer.from(input);
+            let b = Buffer.alloc(8);
+            b.writeBigUInt64LE(BigInt(this.plain_name_length));
+            let ret = Buffer.concat([b, Buffer.from(this.plain_name, 'ascii'), inputBuffer]);
+            return new Uint8Array(ret);
+        }
+    }
+
+    private static locatorToURL(locator : {}) {
+        let portStr = (locator["port"]>0)?(":"+locator["port"]):"";
+        return `amqp://${locator["username"]}:${locator["password"]}@${locator["host"]}${portStr}`;
+    }
+
+    async listen(locatorStr : string, topic : string, callback : (topic: string, data: Message<{}>) => void) {
+        let locator = TMHelper.parseLocator(locatorStr);
+        const url = TMHelper.locatorToURL(locator);
+        let connection = await amqp.connect(url);
+        let channel = await connection.createChannel();
+        channel.assertExchange(locator["identifier"], "topic", {
+            durable : (locator["properties"]["durable"] == "true")
+        });
+        let queue = await channel.assertQueue('', {exclusive: true});
+        channel.bindQueue(queue.queue, locator["identifier"], topic);
+        let parser = this.incoming_type;
+        channel.consume(queue.queue, function(msg) {
+            if (msg.content) {
+                let data = parser.decode(msg.content);
+                callback(topic, data);
+            }
+        });
+    }
+
+    async send_request(locatorStr : string, request : {}, callback : (isFinal: boolean, reply: Message<{}>) => void) {
+        let locator = TMHelper.parseLocator(locatorStr);
+        const url = TMHelper.locatorToURL(locator);
+        let connection = await amqp.connect(url);
+        let channel = await connection.createChannel();
+        let queue = await channel.assertQueue('', {exclusive:true});
+        let correlationId = uuidv4();
+        let parser = this.incoming_type;
+        channel.consume(queue.queue, function(reply) {
+            if (reply.properties.correlationId == correlationId) {
+                let encoding = reply.properties.contentEncoding;
+                if (encoding == 'with_final') {
+                    let finalFlag = (reply.content[reply.content.byteLength-1] != 0);
+                    let content = parser.decode(reply.content.slice(0, reply.content.byteLength-1));
+                    callback(finalFlag, content);
+                    if (finalFlag) {
+                        setTimeout(function() {
+                            connection.close();
+                            process.exit(0);
+                        }, 500);
+                    }
+                } else {
+                    let content = parser.decode(reply.content);
+                    callback(false, content);
+                }
+            }
+        }, {noAck : true});
+        let buffer = this.outgoing_type.encode(this.outgoing_type.create(request)).finish();
+        let sendBuffer = this.attach_identity(buffer);
+        channel.sendToQueue(
+            locator["identifier"]
+            , Buffer.from(sendBuffer)
+            , {
+                correlationId : correlationId
+                , replyTo: queue.queue
+            }
+        );
+    }
+}
+/*
+(async () => {
+    var helper = new TMHelper();
+    await helper.set_incoming_type('../proto/defs.proto', 'simple_demo.InputData');
+    await helper.listen(
+        'localhost::guest:guest:amq.topic[durable=true]'
+        , 'input.data'
+        , function(topic:string, data:Message<{}>) {
+            let obj = {topic: topic, data: data};
+            console.log(obj);
+        }
+    );
+})();*/
