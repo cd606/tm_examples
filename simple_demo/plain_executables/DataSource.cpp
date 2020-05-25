565878b21cf2c4923630c7047246acb340d6b678
cd606 <64788159+cd606@users.noreply.github.com>
2020-05-25 18:16:12 -0500

    start adding heartbeat

--- simple_demo/plain_executables/DataSource.cpp
+++ simple_demo/plain_executables/DataSource.cpp
@@ -11,6 +11,7 @@
 #include <tm_kit/transport/BoostUUIDComponent.hpp>
 #include <tm_kit/transport/zeromq/ZeroMQComponent.hpp>
 #include <tm_kit/transport/zeromq/ZeroMQImporterExporter.hpp>
+#include <tm_kit/transport/HeartbeatComponent.hpp>
 
 #include "defs.pb.h"
 #include "simple_demo/external_logic/DataSource.hpp"
@@ -26,7 +27,8 @@ using TheEnvironment = infra::Environment<
     basic::TrivialBoostLoggingComponent,
     basic::real_time_clock::ClockComponent,
     transport::BoostUUIDComponent,
-    transport::zeromq::ZeroMQComponent
+    transport::zeromq::ZeroMQComponent,
+    transport::HeartbeatComponent
 >;
 using M = infra::RealTimeMonad<TheEnvironment>;
 
@@ -50,6 +52,9 @@ public:
 
 int main(int argc, char **argv) {
     TheEnvironment env;
+    transport::HeartbeatComponentInitializer<TheEnvironment,transport::zeromq::ZeroMQComponent>()
+        (&env, "simple_demo DataSource", transport::ConnectionLocator::parse("localhost:23456"));
+    env.setStatus("program", transport::HeartbeatMessage::Status::Good);
     infra::MonadRunner<M> r(&env);
 
     auto addTopic = basic::SerializationActions<M>::template addConstTopic<InputData>("input.data");
@@ -64,6 +69,8 @@ int main(int argc, char **argv) {
     r.exportItem("publisher", publisher
         , r.execute("addTopic", addTopic
             , r.importItem("source", source)));
+
+    transport::attachHeartbeatComponent(r, &env, "simple_demo.data_source.heartbeat", std::chrono::seconds(10));
 
     r.finalize();
 
