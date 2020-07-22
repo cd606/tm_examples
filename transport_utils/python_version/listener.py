071d4bda4bbc5a0c14574efb30912350be510037
cd606 <64788159+cd606@users.noreply.github.com>
2020-07-21 21:30:34 -0500

    more updates on python TMTransport (work in progress)

--- transport_utils/python_version/listener.py
+++ transport_utils/python_version/listener.py
@@ -7,6 +7,11 @@ from enum import Enum
 from typing import Callable
 from datetime import datetime
 import cbor
+import aiofiles
+
+import click
+
+count : int = 0
 
 async def printQueueData(qin : asyncio.Queue, printMode : str = "length"):
     printFunc : Callable[[bytes], str] = lambda x : ""
@@ -16,6 +21,10 @@ async def printQueueData(qin : asyncio.Queue, printMode : str = "length"):
         printFunc = lambda x : x.decode('utf-8')
     elif printMode == "cbor":
         printFunc = lambda x : repr(cbor.loads(x))
+    elif printMode == "bytes":
+        printFunc = lambda x : repr(x)
+
+    global count
 
     #Please note that Python's protobuf module does not support
     #dynamic decoding, therefore the dynamic protobuf printing 
@@ -23,21 +32,54 @@ async def printQueueData(qin : asyncio.Queue, printMode : str = "length"):
 
     while True:
         item : Tuple[string, bytes] = await qin.get()
+        count = count+1
         if item:
             now = datetime.now()
             print(f"{now.strftime('%Y-%m-%d %H:%M:%S.%f')}: topic '{item[0]}': {printFunc(item[1])}")
 
-async def asyncMain() :
-    printQueue = asyncio.Queue()
-    TMTransport.MultiTransportListener.input(
-        #"rabbitmq://127.0.0.1::guest:guest:amq.topic[durable=true]"
-        #"multicast://224.0.0.114:12346"
-        #"redis://127.0.0.1:6379"
-        "zeromq://localhost:12345"
-        , [printQueue]
-        , topic = "input.data"
-    )
-    await printQueueData(printQueue, printMode="length")
+@click.command()
+@click.option('--address', help='PROTOCOL://LOCATOR')
+@click.option('--topic', help='topic to subscribe', default=None)
+@click.option('--printMode', help='length|string|cbor|none|bytes', default='length')
+@click.option('--summaryPeriod', help='summary period in seconds', default=0)
+@click.option('--captureFile', help='capture file name', default=None)
+
+def run(address : str, topic : str, printmode : str, summaryperiod : int, capturefile : str):
+    async def asyncMain() :
+        if summaryperiod > 0:
+            async def printSummary():
+                global count
+                while True:
+                    await asyncio.sleep(summaryperiod)
+                    now = datetime.now()
+                    print(f"{now.strftime('%Y-%m-%d %H:%M:%S.%f')}: Received {count} messages so far")
+            asyncio.create_task(printSummary())
+        printQueue = asyncio.Queue()
+        qouts = [printQueue]
+        if capturefile:
+            captureQueue = asyncio.Queue()
+            async def writeCaptureFile():
+                async with aiofiles.open(capturefile, "wb") as o:
+                    await o.write(b'\x01\x23\x45\x67')
+                    while True:
+                        topic, data = await captureQueue.get()
+                        now = datetime.now()
+                        t : int = int(now.timestamp()*1000.0)+(now.microsecond//1000)
+                        l : int = len(topic)
+                        ld : int = len(data)
+                        writeData = b'\x76\x54\x32\x10'\
+                                    +t.to_bytes(8, byteorder='little')\
+                                    +l.to_bytes(4, byteorder='little')\
+                                    +topic.encode()\
+                                    +ld.to_bytes(4, byteorder='little')\
+                                    +data\
+                                    +b'\x00'
+                        await o.write(writeData)
+            asyncio.create_task(writeCaptureFile())
+            qouts = [printQueue, captureQueue]
+        TMTransport.MultiTransportListener.input(address, qouts, topic = topic)
+        await printQueueData(printQueue, printMode=printmode)
+    asyncio.run(asyncMain())
 
 if __name__ == "__main__":
-    asyncio.run(asyncMain())
+    run()
