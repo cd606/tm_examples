73bdd1f97255cb73084703d4b2d0ff56943d1ebe
cd606 <64788159+cd606@users.noreply.github.com>
2020-08-13 17:43:46 -0500

    allow macro-generated struct to serialization without field names

--- db_one_list_subscription/DBData.hpp
+++ db_one_list_subscription/DBData.hpp
@@ -18,8 +18,8 @@ namespace db_one_list_subscription {
     TM_BASIC_CBOR_CAPABLE_STRUCT(db_data, DBDataFields);
 }
 
-TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE(db_one_list_subscription::db_key, DBKeyFields);
-TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE(db_one_list_subscription::db_data, DBDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(db_one_list_subscription::db_key, DBKeyFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(db_one_list_subscription::db_data, DBDataFields);
 
 #define DBItemFields \
     ((db_one_list_subscription::db_key, key)) \
