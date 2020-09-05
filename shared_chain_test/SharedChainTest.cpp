84b67cd2c9fd3a1fcfd31679e0db545254719263
cd606 <64788159+cd606@users.noreply.github.com>
2020-09-05 11:35:05 -0500

    more updates on shared chain (work in progress)

--- shared_chain_test/SharedChainTest.cpp
+++ shared_chain_test/SharedChainTest.cpp
@@ -10,6 +10,9 @@
 #include <tm_kit/basic/SerializationHelperMacros.hpp>
 #include <tm_kit/basic/single_pass_iteration_clock/ClockComponent.hpp>
 #include <tm_kit/basic/real_time_clock/ClockComponent.hpp>
+#include <tm_kit/basic/single_pass_iteration_clock/ClockImporter.hpp>
+#include <tm_kit/basic/real_time_clock/ClockImporter.hpp>
+#include <tm_kit/basic/CommonFlowUtils.hpp>
 
 #include <tm_kit/transport/CrossGuidComponent.hpp>
 
@@ -25,62 +28,88 @@ TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE(TransferRequest, TransferRequestFields);
 
 using Process = basic::ConstType<1>;
 
-using ChainItemData = std::variant<TransferRequest, Process>;
-using ChainItemID = std::array<unsigned char, 16>;
+using RequestData = basic::CBOR<std::variant<TransferRequest, Process>>;
+using RequestID = std::string;
+
+#define RequestChainItemFields \
+    ((RequestID, requestID)) \
+    ((RequestData, requestData)) 
+
+TM_BASIC_CBOR_CAPABLE_STRUCT_DEF(RequestChainItem, RequestChainItemFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE(RequestChainItem, RequestChainItemFields);
 
 struct State {
     uint32_t a, b;
     int32_t a_pending, b_pending;
     uint16_t pendingRequestCount;
+
+    State() : a(0), b(0), a_pending(0), b_pending(0), pendingRequestCount(0) {}
+    static State initState() {
+        State s;
+        s.a = 1000;
+        s.b = 1000;
+        return s;
+    }
 };
 
-using ChainItem = basic::CBOR<std::variant<
-    TransferRequest, Process
->>;
+inline std::ostream &operator<<(std::ostream &os, State const &s) {
+    os << "{a=" << s.a << ",b=" << s.b << ",a_pending=" << s.a_pending
+        << ",b_pending=" << s.b_pending << ",pendingRequestCount=" << s.pendingRequestCount
+        << "}";
+    return os;
+}
 
 class InMemoryChain {
 public:
-    struct Item {
-        ChainItemData data;
-        std::atomic<Item *> next;
+    struct MapData {
+        RequestChainItem data;
+        RequestID nextID;
 
-        Item() : data(), next(nullptr) {}
-        Item(Item const &x) : data(x.data), next(x.next.load()) {}
-        Item &operator=(Item const &x) {
-            if (this == &x) {
-                return *this;
-            }
-            data = x.data;
-            next.store(x.next.load());
-            return *this;
-        }
+        MapData() : data(), nextID() {}
+        MapData(RequestChainItem &&d) : data(std::move(d)), nextID() {}
     };
-    using ItemType = Item;
+    using TheMap = std::unordered_map<RequestID, MapData>;
+    using ItemType = RequestChainItem;
 private:
-    Item start_;
+    TheMap theMap_;
+    std::mutex mutex_;
 public:
-    InMemoryChain() : start_() {}
-    Item head(void *) {
-        return start_;
+    InMemoryChain() : theMap_({{"", MapData {}}}), mutex_() {
     }
-    bool fetchNext(Item *current) {
-        if (!current) {
-            return false;
-        }
-        auto *p = current->next.load(std::memory_order_acquire);
-        if (p) {
-            *current = *p;
-            return true;
-        } else {
-            return false;
-        }
+    ItemType head(void *) {
+        std::lock_guard<std::mutex> _(mutex_);
+        return theMap_[""].data;
     }
-    bool append(Item *current, Item *toBeWritten) {
-        if (!current) {
+    std::optional<ItemType> fetchNext(ItemType const &current) {
+        std::lock_guard<std::mutex> _(mutex_);
+        auto iter = theMap_.find(current.requestID);
+        if (iter == theMap_.end()) {
+            return std::nullopt;
+        }
+        if (iter->second.nextID == "") {
+            return std::nullopt;
+        }
+        iter = theMap_.find(iter->second.nextID);
+        if (iter == theMap_.end()) {
+            return std::nullopt;
+        }
+        return {iter->second.data};
+    }
+    bool appendAfter(ItemType const &current, ItemType &&toBeWritten) {
+        std::lock_guard<std::mutex> _(mutex_);
+        if (theMap_.find(toBeWritten.requestID) != theMap_.end()) {
             return false;
         }
-        Item *oldVal = nullptr;
-        return current->next.compare_exchange_strong(oldVal, toBeWritten, std::memory_order_release, std::memory_order_acquire);
+        auto iter = theMap_.find(current.requestID);
+        if (iter == theMap_.end()) {
+            return false;
+        }
+        if (iter->second.nextID != "") {
+            return false;
+        }
+        iter->second.nextID = toBeWritten.requestID;
+        theMap_.insert({iter->second.nextID, MapData {std::move(toBeWritten)}}).first;
+        return true;
     }
 };
 
@@ -88,22 +117,82 @@ class StateFolder {
 public:
     using ResultType = State;
     State initialize(void *) {
-        return State {};
+        return State::initState();
     } 
-    State fold(State const &lastState, InMemoryChain::Item const &newInfo) {
-        return lastState;
+    State fold(State const &lastState, RequestChainItem const &newInfo) {
+        return std::visit([&lastState](auto const &x) -> State {
+            using T = std::decay_t<decltype(x)>;
+            if constexpr (std::is_same_v<T, TransferRequest>) {
+                State newState = lastState;
+                if (x.from == "a") {
+                    newState.a_pending -= x.amount;
+                } else if (x.from == "b") {
+                    newState.b_pending -= x.amount;
+                }
+                if (x.to == "a") {
+                    newState.a_pending += x.amount;
+                } else if (x.to == "b") {
+                    newState.b_pending += x.amount;
+                }
+                ++(newState.pendingRequestCount);
+                return newState;
+            } else if constexpr (std::is_same_v<T, Process>) {
+                State newState = lastState;
+                newState.a += newState.a_pending;
+                newState.a_pending = 0;
+                newState.b += newState.b_pending;
+                newState.b_pending = 0;
+                newState.pendingRequestCount = 0;
+                return newState;
+            } else {
+                return lastState;
+            }
+        }, newInfo.requestData.value);
     }
 };
 
+template <class App>
 class RequestHandler {
 public:
     using ResponseType = bool;
-    using InputType = int;
-    void initialize(void *) {
-
+    using InputType = RequestData;
+    using Env = typename App::EnvironmentType;
+    void initialize(Env *) {
     }
-    InMemoryChain::Item *handleInput(InputType &&, State const &, bool *) {
-        return nullptr;
+    std::tuple<ResponseType, std::optional<RequestChainItem>> handleInput(Env *env, typename App::Key<InputType> &&input, State const &currentState) {
+        auto idStr = Env::id_to_string(input.id());
+        return std::visit([env,&idStr,&currentState](auto &&x) -> std::tuple<ResponseType, std::optional<RequestChainItem>> {
+            using T = std::decay_t<decltype(x)>;
+            if constexpr (std::is_same_v<T, TransferRequest>) {
+                if (currentState.pendingRequestCount >= 10) {
+                    env->log(infra::LogLevel::Warning, "too many pending requests");
+                    return {false, std::nullopt};
+                }
+                if ((x.from != "a" && x.from != "b") || (x.to != "a" && x.to != "b")) {
+                    env->log(infra::LogLevel::Warning, "can only transfer between a and b");
+                    return {false, std::nullopt};
+                }
+                if (x.from == "a" && (static_cast<int64_t>(currentState.a)+static_cast<int64_t>(currentState.a_pending-x.amount) < 0)) {
+                    env->log(infra::LogLevel::Warning, "not enough amount "+std::to_string(x.amount)+" in a");
+                    return {false, std::nullopt};
+                }
+                if (x.from == "b" && (static_cast<int64_t>(currentState.b)+static_cast<int64_t>(currentState.b_pending-x.amount) < 0)) {
+                    env->log(infra::LogLevel::Warning, "not enough amount "+std::to_string(x.amount)+" in b");
+                    return {false, std::nullopt};
+                }
+                std::ostringstream oss;
+                oss << "Appending request " << x;
+                env->log(infra::LogLevel::Info, oss.str());
+                return {true, {RequestChainItem {idStr, {std::move(x)}}}};
+            } else if constexpr (std::is_same_v<T, Process>) {
+                std::ostringstream oss;
+                oss << "Appending request " << x;
+                env->log(infra::LogLevel::Info, oss.str());
+                return {true, {RequestChainItem {idStr, {std::move(x)}}}};
+            } else {
+                return {false, std::nullopt};
+            }
+        }, std::move(input.key().value));
     }
 };
 
@@ -111,16 +200,75 @@ void histRun() {
     using TheEnvironment = infra::Environment<
         infra::CheckTimeComponent<true>,
         infra::FlagExitControlComponent,
-        basic::TimeComponentEnhancedWithSpdLogging<basic::single_pass_iteration_clock::ClockComponent<std::chrono::system_clock::time_point>>,
+        basic::TimeComponentEnhancedWithSpdLogging<basic::single_pass_iteration_clock::ClockComponent<std::chrono::system_clock::time_point>, false>,
         transport::CrossGuidComponent
     >;
     using A = infra::SinglePassIterationApp<TheEnvironment>;
 
     TheEnvironment env;
+    infra::AppRunner<A> r(&env);
+
     InMemoryChain chain;
 
-    basic::simple_shared_chain::ChainWriter<A, InMemoryChain, StateFolder, RequestHandler> w(&chain);
-    basic::simple_shared_chain::ChainReader<A, InMemoryChain, StateFolder> r(&env, &chain);
+    std::srand(std::time(nullptr));
+    
+    auto readerClockImporter = basic::single_pass_iteration_clock::ClockImporter<TheEnvironment>::createRecurringClockImporter<basic::VoidStruct>(
+        infra::withtime_utils::parseLocalTime("2020-01-01T10:00:00")
+        , infra::withtime_utils::parseLocalTime("2020-01-01T11:00:00")
+        , std::chrono::minutes(1)
+        , [](typename TheEnvironment::TimePointType const &tp) {
+            return basic::VoidStruct {};
+        }
+    );
+    r.registerImporter("readerClockImporter", readerClockImporter);
+
+    auto readerAction = A::liftMaybe<basic::VoidStruct>(basic::simple_shared_chain::ChainReader<A, InMemoryChain, StateFolder>(&env, &chain));
+    r.registerAction("readerAction", readerAction);
+
+    auto printState = A::pureExporter<State>(
+        [&env](State &&s) {
+            std::ostringstream oss;
+            oss << "Current state: " << s;
+            env.log(infra::LogLevel::Info, oss.str());
+        }
+    );
+    r.registerExporter("printState", printState);
+    r.exportItem(printState, r.execute(readerAction, r.importItem(readerClockImporter)));
+
+    auto reqClockImporter = basic::single_pass_iteration_clock::ClockImporter<TheEnvironment>::createRecurringClockImporter<RequestData>(
+        infra::withtime_utils::parseLocalTime("2020-01-01T10:00:00")
+        , infra::withtime_utils::parseLocalTime("2020-01-01T11:00:00")
+        , std::chrono::seconds(5)
+        , [](typename TheEnvironment::TimePointType const &tp) -> RequestData {
+            int x = std::rand()%5;
+            if (x <= 1) {
+                TransferRequest req;
+                req.from = "a";
+                req.to = "b";
+                req.amount = (std::rand()%9+1)*100;
+                return {{req}};
+            } else if (x <= 3) {
+                TransferRequest req;
+                req.from = "b";
+                req.to = "a";
+                req.amount = (std::rand()%9+1)*100;
+                return {{req}};
+            } else {
+                return {{Process {}}};
+            }
+        }
+    );
+    r.registerImporter("reqClockImporter", reqClockImporter);
+
+    auto keyify = A::kleisli<RequestData>(basic::CommonFlowUtilComponents<A>::keyify<RequestData>());
+    r.registerAction("keyify", keyify);
+
+    auto reqHandler = A::fromAbstractOnOrderFacility<RequestData, bool>(new basic::simple_shared_chain::ChainWriter<A, InMemoryChain, StateFolder, RequestHandler<A>>(&chain));
+    r.registerOnOrderFacility("reqHandler", reqHandler);
+
+    r.placeOrderWithFacilityAndForget(r.execute(keyify, r.importItem(reqClockImporter)), reqHandler);
+
+    r.finalize();
 }
 
 int main(int argc, char **argv) {
