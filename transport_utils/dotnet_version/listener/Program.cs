398d37f6d0281b8364fd5c500917e71977c51220
cd606 <64788159+cd606@users.noreply.github.com>
2020-10-31 18:46:58 -0500

    more updates to dotnet version

--- transport_utils/dotnet_version/listener/Program.cs
+++ transport_utils/dotnet_version/listener/Program.cs
@@ -5,6 +5,7 @@ using Dev.CD606.TM.Infra.RealTimeApp;
 using Dev.CD606.TM.Basic;
 using Dev.CD606.TM.Transport;
 using Microsoft.Extensions.CommandLineUtils;
+using PeterO.Cbor;
 
 namespace listener
 {
@@ -25,24 +26,59 @@ namespace listener
             var importer = MultiTransportImporter<ClockEnv>.CreateImporter(
                 address, topic
             );
+            var count = 0;
             var exporter = RealTimeAppUtils<ClockEnv>.pureExporter<ByteDataWithTopic>(
                 (x) => {
+                    ++count;
                     switch (printMode)
                     {
                         case PrintMode.Length:
                             env.log(LogLevel.Info, $"Topic {x.topic}: {x.content.Length} bytes");
                             break;
                         case PrintMode.String:
-                            env.log(LogLevel.Info, $"Topic {x.topic}: {Encoding.x.content}");
+                            env.log(LogLevel.Info, $"Topic {x.topic}: {Encoding.UTF32.GetString(x.content)}");
+                            break;
+                        case PrintMode.Cbor:
+                            env.log(LogLevel.Info, $"Topic {x.topic}: {CBORObject.DecodeFromBytes(x.content)}");
+                            break;
+                        case PrintMode.None:
+                        default:
                             break;
                     }
-                    
                 }
                 , false
             );
             r.exportItem(exporter, r.importItem(importer));
+            if (!captureFile.Equals(""))
+            {
+                var fileExporter = FileUtils<ClockEnv>.byteDataWithTopicOutput(
+                    captureFile
+                    , filePrefix : new ByteData(new byte[] {0x01, 0x23, 0x45, 0x67})
+                    , recordPrefix : new ByteData(new byte[] {0x76, 0x54, 0x32, 0x10})
+                );
+                r.exportItem(fileExporter, r.importItem(importer));
+            }
+            if (summaryPeriod != 0)
+            {
+                var now = env.now();
+                var timerImporter = ClockImporter<ClockEnv>.createRecurringClockConstImporter<int>(
+                    now
+                    , now.AddDays(1)
+                    , summaryPeriod*1000
+                    , 0
+                );
+                var summaryExporter = RealTimeAppUtils<ClockEnv>.pureExporter<int>(
+                    (x) => {
+                        env.log(LogLevel.Info, $"Read {count} messages so far");
+                    }
+                    , false
+                );
+                r.exportItem(summaryExporter, r.importItem(timerImporter));
+            }
             r.finalize();
-            RealTimeAppUtils<ClockEnv>.runForever(env);
+            RealTimeAppUtils<ClockEnv>.terminateAfterDuration(
+                env, TimeSpan.FromDays(1)
+            );
         }
         static void Main(string[] args)
         {
