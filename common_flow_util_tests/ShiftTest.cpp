459973ff03744c193146867fbefbfa6bc510064d
cd606 <cd606dc@gmail.com>
2021-05-24 15:51:42 -0500

    add some tests

---
+++ common_flow_util_tests/ShiftTest.cpp
@@ -0,0 +1,86 @@
+#include <tm_kit/infra/WithTimeData.hpp>
+#include <tm_kit/infra/SinglePassIterationApp.hpp>
+#include <tm_kit/infra/Environments.hpp>
+#include <tm_kit/infra/TerminationController.hpp>
+#include <tm_kit/basic/IntIDComponent.hpp>
+#include <tm_kit/basic/CommonFlowUtils.hpp>
+
+using namespace dev::cd606::tm::infra;
+
+struct TrivialLoggingComponent {
+    static inline void log(LogLevel l, std::string const &s) {
+        std::cout << l << ": " << s << std::endl;
+    }
+};
+
+template <class TimeComponent>
+using BasicEnvironment = Environment<
+    dev::cd606::tm::basic::IntIDComponent<uint8_t>,
+    CheckTimeComponent<true>,
+    FlagExitControlComponent,
+    TrivialLoggingComponent,
+    TimeComponent
+    >;
+
+struct FakeClockComponent {
+    using TimePointType = uint64_t;
+    static constexpr bool PreserveInputRelativeOrder = true;
+    static uint64_t resolveTime() {
+        return 0;
+    }
+    static uint64_t resolveTime(uint64_t triggeringInputTime) {
+        return triggeringInputTime;
+    }
+};
+
+int main() {
+    using M = SinglePassIterationApp<BasicEnvironment<FakeClockComponent>>;
+    using R = AppRunner<M>;
+    M::EnvironmentType env;
+    R r(&env);
+
+    using CFU = dev::cd606::tm::basic::CommonFlowUtilComponents<M>;
+
+    auto source = M::simpleImporter<int>(
+        [](M::StateType *env) -> M::Data<int> {
+            static int ii = -1;
+            ++ii;
+            std::variant<int,int,std::string> x;
+            return M::InnerData<int> {
+                env 
+                , {
+                    (M::TimePoint) ii
+                    , ii
+                    , ii >= 10
+                }
+            };
+        }
+    );
+    r.registerImporter("source", source);
+
+    auto action = CFU::leftShift<int>(2);
+    r.registerAction("action", action);
+
+    auto bunch = CFU::bunch<int>();
+    r.registerAction("bunch", bunch);
+
+    auto print = M::simpleExporter<std::vector<int>>(
+        [](M::InnerData<std::vector<int>> &&d) {
+            std::ostringstream oss;
+            oss << "Time " << d.timedData.timePoint
+                << ", data [";
+            for (auto x : d.timedData.value) {
+                oss << x << ' ';
+            }
+            oss << "]" << (d.timedData.finalFlag?", final":"");
+            d.environment->log(LogLevel::Info, oss.str());
+        }
+    );
+    r.registerExporter("print", print);
+
+    r.execute(bunch, r.execute(action, r.importItem(source)));
+    r.execute(bunch, r.importItem(source));
+    r.exportItem(print, r.actionAsSource(bunch));
+
+    r.finalize();
+}
---
+++ common_flow_util_tests/Synchronizer2Test.cpp
@@ -0,0 +1,116 @@
+#include <tm_kit/infra/WithTimeData.hpp>
+#include <tm_kit/infra/SinglePassIterationApp.hpp>
+#include <tm_kit/infra/Environments.hpp>
+#include <tm_kit/infra/TerminationController.hpp>
+#include <tm_kit/basic/IntIDComponent.hpp>
+#include <tm_kit/basic/CommonFlowUtils.hpp>
+#include <tm_kit/basic/AppRunnerUtils.hpp>
+#include <tm_kit/basic/CounterComponent.hpp>
+#include <tm_kit/transport/CrossGuidComponent.hpp>
+#include <tm_kit/transport/BoostUUIDComponent.hpp>
+
+using namespace dev::cd606::tm::infra;
+using namespace dev::cd606::tm::basic;
+using namespace dev::cd606::tm::transport;
+
+struct TrivialLoggingComponent {
+    static inline void log(LogLevel l, std::string const &s) {
+        std::cout << l << ": " << s << std::endl;
+    }
+};
+
+template <class TimeComponent>
+using BasicEnvironment = Environment<
+    CheckTimeComponent<true>,
+    FlagExitControlComponent,
+    TrivialLoggingComponent,
+    TimeComponent,
+    BoostUUIDComponent,
+    CounterComponent<>
+    >;
+
+struct FakeClockComponent {
+    using TimePointType = uint64_t;
+    static constexpr bool PreserveInputRelativeOrder = true;
+    static uint64_t resolveTime() {
+        return 0;
+    }
+    static uint64_t resolveTime(uint64_t triggeringInputTime) {
+        return triggeringInputTime;
+    }
+};
+
+int main() {
+    using M = SinglePassIterationApp<BasicEnvironment<FakeClockComponent>>;
+    using R = AppRunner<M>;
+    M::EnvironmentType env;
+    R r(&env);
+
+    using CFU = CommonFlowUtilComponents<M>;
+    using ARU = AppRunnerUtilComponents<R>;
+    using KU = KleisliUtils<M>;
+
+    auto source = M::simpleImporter<int>(
+        [](M::StateType *env) -> M::Data<int> {
+            static int ii = -1;
+            ++ii;
+            std::variant<int,int,std::string> x;
+            return M::InnerData<int> {
+                env 
+                , {
+                    (M::TimePoint) ii
+                    , ii
+                    , ii >= 10
+                }
+            };
+        }
+    );
+    r.registerImporter("source", source);
+
+    using IntP = std::shared_ptr<const int>;
+    
+    auto share = KU::action(CFU::shareBetweenDownstream<int>());
+    r.registerAction("share", share);
+
+    auto facility1 = M::liftPureOnOrderFacility<IntP>(
+        [](IntP &&a) -> SingleLayerWrapperWithID<1,int> {
+            return {*a+1};
+        }
+    );
+    auto facility2 = M::liftPureOnOrderFacility<IntP>(
+        [](IntP &&a) -> SingleLayerWrapperWithID<2,int> {
+            return {*a+2};
+        }
+    );
+    auto keyify = KU::action(CFU::keyifyThroughCounter<IntP>());
+    r.registerOnOrderFacility("facility1", facility1);
+    r.registerOnOrderFacility("facility2", facility2);
+    r.registerAction("keyify", keyify);
+
+    auto combinedFacility = ARU::facilitoidSynchronizer2<
+        IntP
+        , SingleLayerWrapperWithID<1,int>
+        , SingleLayerWrapperWithID<2,int>
+    >(
+        r.facilityConnector(facility1)
+        , r.facilityConnector(facility2)
+        , [](auto &&key, auto &&x1, auto &&x2) -> std::tuple<int,int> {
+            return {std::move(x1.value), std::move(x2.value)};
+        }
+        , "combined"
+    );
+
+    auto keyedSource = r.execute(keyify, r.execute(share, r.importItem(source)));
+    auto print = M::pureExporter<M::KeyedData<IntP,std::tuple<int,int>>>(
+        [&env](M::KeyedData<IntP,std::tuple<int,int>> &&d) {
+            std::ostringstream oss;
+            oss << d.key.id() << ' ' << *(d.key.key()) << ": " << std::get<0>(d.data) << ", " << std::get<1>(d.data);
+            env.log(LogLevel::Info, oss.str());
+        }
+    );
+    r.registerExporter("print", print);
+
+    combinedFacility(r, keyedSource.clone(), r.exporterAsSink(print));
+
+    r.finalize();
+}
