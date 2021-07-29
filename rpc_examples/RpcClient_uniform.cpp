2080d0e18d60131d0e45a07d819da5cb7824b91f
cd606 <cd606dc@gmail.com>
2021-07-29 15:30:31 -0500

    add more rpc examples

---
+++ rpc_examples/RpcClient_uniform.cpp
@@ -0,0 +1,67 @@
+#include <tm_kit/infra/Environments.hpp>
+#include <tm_kit/infra/SynchronousRunner.hpp>
+
+#include <tm_kit/basic/SpdLoggingComponent.hpp>
+#include <tm_kit/basic/real_time_clock/ClockComponent.hpp>
+
+#include <tm_kit/transport/CrossGuidComponent.hpp>
+#include <tm_kit/transport/MultiTransportRemoteFacilityManagingUtils_SynchronousRunner.hpp>
+
+#include "RpcInterface.hpp"
+
+using namespace dev::cd606::tm;
+
+using Environment = infra::Environment<
+    infra::CheckTimeComponent<true>,
+    infra::TrivialExitControlComponent,
+    basic::TimeComponentEnhancedWithSpdLogging<basic::real_time_clock::ClockComponent>,
+    transport::CrossGuidComponent,
+    transport::AllNetworkTransportComponents
+>;
+using M = infra::RealTimeApp<Environment>;
+using SR = infra::SynchronousRunner<M>;
+
+int main() {
+    Environment env;
+    SR r(&env);
+
+    std::vector<std::tuple<
+        std::string, std::string, bool
+    >> descriptors {
+        {"redis://127.0.0.1:6379:::rpc_example_simple", "SIMPLE RPC", false}
+        , {"redis://127.0.0.1:6379:::rpc_example_client_stream", "CLIENT STREAM RPC", true}
+        , {"redis://127.0.0.1:6379:::rpc_example_server_stream", "SERVER STREAM RPC", false}
+        , {"redis://127.0.0.1:6379:::rpc_example_both_stream", "BOTH STREAM RPC", true}
+    };
+
+    for (auto const &desc : descriptors) {
+        std::cout << "=============" << std::get<1>(desc) << "====================\n";
+        auto streamer = r.facilityStreamer(
+            transport::MultiTransportRemoteFacilityManagingUtils<SR>
+                ::setupSimpleRemoteFacility<rpc_examples::Input, rpc_examples::Output>(
+                std::get<0>(desc)
+            )
+        );
+        streamer << rpc_examples::Input {5, "abc"};
+        if (std::get<2>(desc)) {
+            streamer << rpc_examples::Input {-1, "bcd"};
+            streamer << rpc_examples::Input {-2, "cde"};
+            streamer << rpc_examples::Input {-3, "def"};
+            streamer << rpc_examples::Input {-4, "efg"}; //total 5 inputs for the ones 
+                                                         //requiring client-side streaming
+                                                         //(as indicated by the first input)
+        }
+        //The two ways of accessing the result below are equivalent in this case.
+        //The first way would be more useful if more control (e.g. timeout, 
+        //interleaving read/write) is desired, while the second way is more straightforward.
+        /*
+        while (!streamer->empty()) {
+            std::cout << streamer->front().timedData.value.data << '\n';
+            streamer->pop_front();
+        }
+        */
+        for (auto const &d : *streamer) {
+            std::cout << d.timedData.value.data << '\n';
+        }
+    }
+}
---
+++ rpc_examples/dotnet_client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dev.CD606.TM.Infra.RealTimeApp;
+using Dev.CD606.TM.Basic;
+using Dev.CD606.TM.Transport;
+using PeterO.Cbor;
+
+namespace dotnet_client
+{
+    [CborWithoutFieldNames]
+    class Input 
+    {
+        public Int32 x {get; set;}
+        public string y {get; set;}
+    }
+    [CborWithoutFieldNames]
+    class Output 
+    {
+        public string result {get; set;}
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var env = new ClockEnv();
+            var r = new SynchronousRunner<ClockEnv>(env);
+
+            var descriptors = new List<(string,string,bool)> {
+                ("redis://127.0.0.1:6379:::rpc_example_simple", "SIMPLE RPC", false)
+                , ("redis://127.0.0.1:6379:::rpc_example_client_stream", "CLIENT STREAM RPC", true)
+                , ("redis://127.0.0.1:6379:::rpc_example_server_stream", "SERVER STREAM RPC", false)
+                , ("redis://127.0.0.1:6379:::rpc_example_both_stream", "BOTH STREAM RPC", true)
+            };
+
+            foreach (var desc in descriptors)
+            {
+                Console.WriteLine($"============={desc.Item2}====================");
+                var streamer = r.facilityStreamer(
+                    MultiTransportFacility<ClockEnv>.CreateFacility<Input,Output>(
+                        (x) => CborEncoder<Input>.Encode(x).EncodeToBytes()
+                        , (b) => CborDecoder<Output>.Decode(CBORObject.DecodeFromBytes(b))
+                        , desc.Item1
+                    )
+                );
+                streamer.Send(new Input() {x=5, y="abc"});
+                if (desc.Item3)
+                {
+                    streamer.Send(new Input() {x=-1, y="bcd"});
+                    streamer.Send(new Input() {x=-2, y="cde"});
+                    streamer.Send(new Input() {x=-3, y="def"});
+                    streamer.Send(new Input() {x=-4, y="efg"});
+                }
+                await foreach (var output in streamer.Results())
+                {
+                    Console.WriteLine(output.timedData.value.data.result);
+                }
+            }
+        }
+    }
+}
