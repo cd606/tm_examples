74ecd29a6ff817bd39be630b832cac8a890421e5
cd606 <cd606dc@gmail.com>
2021-08-09 11:52:45 -0500

    add copy test

--- csv_test/CsvTest.cpp
+++ csv_test/CsvTest.cpp
@@ -4,6 +4,7 @@
 
 #include <tm_kit/basic/SerializationHelperMacros.hpp>
 #include <tm_kit/basic/StructFieldInfoBasedCsvUtils.hpp>
+#include <tm_kit/basic/StructFieldInfoBasedCopy.hpp>
 #include <tm_kit/basic/top_down_single_pass_iteration_clock/ClockComponent.hpp>
 
 #define InsideDataFields \
@@ -24,15 +25,41 @@
         ((std::string, name)) \
         ((int32_t, amount)) \
         ((std::optional<inside_data>, inside)) \
+        ((inside_data, inside2)) \
         (((std::array<inside_data, 5>), moreData)) \
         ((std::tm, theTime)) \
         ((std::chrono::system_clock::time_point, tp))
 #endif
 
+#define SmallInsideDataFields \
+    ((double, i)) \
+    ((std::string, s))
+#define SmallTestDataFields \
+    ((small_inside_data, inside)) \
+    ((std::string, name)) \
+    (((std::array<std::optional<small_inside_data>,3>), moreData)) \
+    ((std::tm, tp))
+#define TestData2Fields \
+    ((std::string, name)) \
+    ((int32_t, amount)) \
+    ((std::optional<inside_data>, inside)) \
+    ((std::string, inside2_s)) \
+    ((double, inside2_stat)) \
+    ((std::optional<int16_t>, inside2_i)) \
+    (((std::array<inside_data, 5>), moreData)) \
+    ((std::tm, theTime)) \
+    ((std::chrono::system_clock::time_point, tp))
+
 TM_BASIC_CBOR_CAPABLE_STRUCT(inside_data, InsideDataFields);
 TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(inside_data, InsideDataFields);
 TM_BASIC_CBOR_CAPABLE_STRUCT(test_data, TestDataFields);
 TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(test_data, TestDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT(small_inside_data, SmallInsideDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(small_inside_data, SmallInsideDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT(small_test_data, SmallTestDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(small_test_data, SmallTestDataFields);
+TM_BASIC_CBOR_CAPABLE_STRUCT(test_data_2, TestData2Fields);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(test_data_2, TestData2Fields);
 
 using namespace dev::cd606::tm;
 
@@ -74,9 +101,19 @@ int main() {
     d.moreData[1].s = "test";
     d.moreData[1].i = 2;
     d.tp = std::chrono::system_clock::now();
+
+    small_test_data sd;
+    basic::struct_field_info_utils::StructuralCopy::copy(sd, d);
+    test_data_2 d2;
+    basic::struct_field_info_utils::FlatCopy::copy(d2, d);
+    std::cout << d << '\n';
+    std::cout << sd << '\n';
+    std::cout << d2 << '\n';
+
     r.exportItem(ex, std::move(d));
 
     std::cout << ss.str() << '\n';
+
 
     /*
     auto im = basic::struct_field_info_utils::StructFieldInfoBasedCsvImporterFactory<M>
