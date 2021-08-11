68e79078e64b8b0e816a34d4a8908f9c7aced887
cd606 <cd606dc@gmail.com>
2021-08-11 18:11:09 -0500

    updates to proto-interop

--- serialization_test/ProtoInteropTest.cpp
+++ serialization_test/ProtoInteropTest.cpp
@@ -14,7 +14,7 @@ using namespace dev::cd606::tm::infra;
 #define INNER_TEST_STRUCT_FIELDS \
     ((int32_t, a)) \
     ((double, b)) \
-    (((SingleLayerWrapperWithID<5,std::vector<std::string>>), c)) \
+    (((SingleLayerWrapperWithID<1001,std::vector<std::string>>), c)) \
     ((std::string, d)) 
 
 #define OUTER_TEST_STRUCT_FIELDS \
@@ -33,8 +33,8 @@ int main(int argc, char **argv) {
     OuterTestStruct s {
         {1.0f, 2.0f, 3.0f}
         , InnerTestStruct {
-            1, 10.0, 
-            {{"abc", "bcd", "cde"}}
+            -37, 10.2525, 
+            {{"abcde", "bcd", "cde"}}
             , "xyz"
         }
         , false
@@ -47,205 +47,11 @@ int main(int argc, char **argv) {
     std::cout << '\n';
     OuterTestStruct s1;
     proto_interop::ProtoDecoder<OuterTestStruct> dec(&s1);
-    proto_interop::internal::FieldHeader fh;
-    auto res = proto_interop::internal::FieldHeaderSupport::readHeader(fh, encoded, 0);
+    auto res = dec.handle(proto_interop::internal::ProtoWireType::LengthDelimited, encoded, 0);
     if (res) {
-        std::cout << "fh success " << *res << '\n';
+        std::cout << "success " << *res << '\n';
+        std::cout << s1 << '\n';
     } else {
-        std::cout << "fh fail\n";
+        std::cout << "fail\n";
     }
 }
-#if 0
-    /*
-    TestStruct s1;
-    auto p = StructFieldTypeInfo<TestStruct, StructFieldInfo<TestStruct>::getFieldIndex("bTest")>::fieldPointer();
-    (s1.*p).push_back("abc");
-    std::cout << (s1.*p)[0] << '\n';
-    std::cout << StructFieldInfo<TestStruct3<std::string,int>>::FIELD_NAMES.size() << '\n';
-    std::cout << StructFieldInfo<TestStruct3<std::string,int>>::FIELD_NAMES[1] << '\n';
-    StructFieldTypeInfo<TestStruct3<std::string,int>, StructFieldInfo<TestStruct3<std::string,int>>::getFieldIndex("bTest")>::TheType x {"a", "b"};
-    auto q = StructFieldTypeInfo<TestStruct3<std::string,int>, StructFieldInfo<TestStruct3<std::string,int>>::getFieldIndex("bTest")>::fieldPointer();
-    TestStruct3<std::string,int> s3;
-    (s3.*q).push_back("bcd");
-    const TestStruct3<std::string,int> s3_1 = s3;
-    std::cout << (s3_1.*q)[0] << '\n';
-    std::cout << x[0] << "\n";
-    std::cout << StructFieldInfo<TestStruct4<std::string,int>>::FIELD_NAMES.size() << '\n';
-    std::cout << StructFieldInfo<TestStruct4<std::string,int>>::getFieldIndex("bTest") << '\n';
-    return 0;
-    */
-    //std::cout << bytedata_utils::DirectlySerializableChecker<TriviallySerializable<std::array<double, 10>>>::IsDirectlySerializable() << '\n';
-    using TestType = 
-        std::tuple<
-            TestEnum //int32_t
-            , double
-            , std::string
-            , std::unique_ptr<ByteDataWithTopic>
-            , VoidStruct 
-            , std::variant<
-                std::vector<uint16_t>
-                , std::optional<SingleLayerWrapper<
-                    std::array<char,5>
-                >>
-            >
-            , GroupedVersionedData<std::string, int64_t, double>
-            , char
-            , std::map<std::string, int32_t>
-            , std::unordered_map<int32_t, double>
-            , ConstType<5>
-            , std::list<int16_t>
-            , std::tuple<std::tuple<bool>>
-            , std::tuple<>
-            , std::tuple<float, double>
-            , TriviallySerializable<std::array<float,5>>
-        >
-    ;
-    char buf[10] = {0x1, 0x2, 0x3, 0x4, 0x5, (char) 0xff, (char) 0xfe, (char) 0xfd, (char) 0xfc, (char) 0xfb};
-    TestType t {
-        TestEnum::Item3 //-5
-        , 2.3E7
-        , "this is a test"
-        , std::make_unique<ByteDataWithTopic>(
-            ByteDataWithTopic {
-                "test.topic"
-                , std::string {buf, buf+10}
-            }
-        )
-        , VoidStruct {}
-        /*
-        , std::vector<uint16_t> {
-            10000, 11000, 12000
-        }*/
-        , std::optional<SingleLayerWrapper<
-                    std::array<char,5>
-                >> {
-            SingleLayerWrapper<
-                std::array<char,5>
-            > {
-                {'a','b','c','d','e'}
-            }
-        }
-        , GroupedVersionedData<std::string, int64_t, double> {
-            "group1", 20, 1111.11
-        }
-        , 'x'
-        , std::map<std::string, int32_t> {
-            {"a", 5}
-            , {"b", 6}
-        }
-        , std::unordered_map<int32_t, double> {
-            {10, 123.456}
-            , {20, 234.567}
-        }
-        , ConstType<5> {}
-        , std::list<int16_t> {321, 654, 987}
-        , std::tuple<std::tuple<bool>> {{true}}
-        , std::tuple<> {}
-        , std::tuple<float, double> {1.2f, 3.4}
-        , TriviallySerializable<std::array<float,5>> {
-            std::array<float, 5> {
-                1.0f, 2.0f, 3.0f, 4.0f, 5.0f
-            }
-        }
-    };
-    char buf1[1024];
-    auto encodedV = bytedata_utils::RunCBORSerializerWithNameList<TestType, 16>::apply(
-        t
-        , {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16"}
-        , buf1
-    );
-    auto encoded = std::string_view(buf1, encodedV);
-    /*
-    auto encoded = bytedata_utils::RunCBORSerializerWithNameList<TestType, 15>::apply(
-        t
-        , {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15"}
-    );*/
-    //auto encoded = bytedata_utils::RunSerializer<CBORWithMaxSizeHint<TestType>>::apply({std::move(t), 1024});
-    //auto encoded = bytedata_utils::RunSerializer<TestType>::apply(t);
-    bytedata_utils::printByteDataDetails(std::cout, ByteDataView {encoded});
-    std::cout << "\n";
-    auto decoded = bytedata_utils::RunCBORDeserializerWithNameList<TestType, 16>::apply(
-        encoded, 0
-        , {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16"}
-    );
-    //auto decoded = bytedata_utils::RunDeserializer<CBORWithMaxSizeHint<TestType>>::apply(encoded);
-    //auto decoded = bytedata_utils::RunDeserializer<TestType>::apply(encoded);
-    if (decoded) {
-        std::cout << "Decode success\n";
-        TestType const &data = std::get<0>(*decoded);
-        //TestType const &data = decoded->value;
-        //TestType const &data = *decoded;
-        PrintHelper<TestType>::print(std::cout, data);
-	    std::cout << '\n';
-        std::cout << "TestType {\n";
-        std::cout << "\t" << std::get<0>(data) << "\n"
-            << "\t, " << std::get<1>(data) << "\n"
-            << "\t, '" << std::get<2>(data) << "'\n";
-        std::cout << "\t, {'" << std::get<3>(data)->topic << "',";
-        bytedata_utils::printByteDataDetails(std::cout, ByteData {std::get<3>(data)->content});
-        std::cout << "}";
-        std::cout << "\n";
-        std::cout << "\t, {}\n";
-        std::cout << "\t, ";
-        switch (std::get<5>(data).index()) {
-        case 0:
-            {
-                std::cout << "[";
-                auto v = std::get<0>(std::get<5>(data));
-                for (auto item : v) {
-                    std::cout << item << ' ';
-                }
-                std::cout << "]";
-            }
-            break;
-        case 1:
-            {
-                auto v = std::get<1>(std::get<5>(data));
-                if (v) {
-                    std::cout << "[";
-                    for (auto item : v->value) {
-                        std::cout << item << ' ';
-                    }
-                    std::cout << "]";
-                } else {
-                    std::cout << "std::nullopt";
-                }
-            }
-            
-        }
-        std::cout << "\n";
-        std::cout << "\t, {" 
-            << std::get<6>(data).groupID 
-            << "," << std::get<6>(data).version 
-            << "," << std::get<6>(data).data << "}\n";
-        std::cout << "\t, " << (std::get<7>(data)?"true":"false") << "\n";
-        std::cout << "\t, {";
-        for (auto const &item : std::get<8>(data)) {
-            std::cout << "{'" << item.first << "'," << item.second << "} ";
-        }
-        std::cout << "}\n";
-        std::cout << "\t, {";
-        for (auto const &item : std::get<9>(data)) {
-            std::cout << "{'" << item.first << "'," << item.second << "} ";
-        }
-        std::cout << "}\n";
-        std::cout << "\t, {}\n";
-        std::cout << "\t, [";
-        for (auto const &item : std::get<11>(data)) {
-            std::cout << item << ' ';
-        }
-        std::cout << "]\n";
-        std::cout << "\t, {{" << std::get<0>(std::get<0>(std::get<12>(data))) << "}}\n";
-        std::cout << "\t, {}\n";
-        std::cout << "\t, {" << std::get<0>(std::get<14>(data)) << "," << std::get<1>(std::get<14>(data)) << "}\n";
-        std::cout << "\t, {[";
-        for (auto const &item : std::get<15>(data).value) {
-            std::cout << item << ' ';
-        }
-        std::cout << "]}\n";
-        std::cout << "}\n";
-    } else {
-        std::cout << "Decode failure\n";
-    }
-}
-#endif
---
+++ serialization_test/proto-interop-dotnet/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using ProtoBuf;
+
+namespace proto_interop_dotnet
+{
+    [ProtoContract]
+    class InnerTestStruct {
+        [ProtoMember(1,DataFormat = DataFormat.ZigZag)]
+        public Int32 a {get; set;}
+        [ProtoMember(2)]
+        public double b {get; set;}
+        [ProtoMember(1001)]
+        public List<string> c {get; set;}
+        [ProtoMember(1002)]
+        public string d {get; set;}
+        public override string ToString()
+        {
+            var x = new StringBuilder();
+            x.Append($"a={a},b={b},c=[");
+            foreach (var y in c) {
+                x.Append(y).Append(' ');
+            }
+            x.Append($"] d={d}}}");
+            return x.ToString();
+        }
+    }
+    [ProtoContract]
+    class OuterTestStruct {
+        [ProtoMember(1)]
+        public List<float> f {get; set;}
+        [ProtoMember(2)]
+        public InnerTestStruct g {get; set;}
+        [ProtoMember(3)]
+        public bool h {get; set;}
+        public override string ToString()
+        {
+            var b = new StringBuilder();
+            b.Append("{f=[");
+            foreach (var x in f) {
+                b.Append(x);
+                b.Append(' ');
+            }
+            b.Append("],g={").Append(g.ToString()).Append("},h=").Append(h).Append("}");
+            return b.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[] {
+                0x0a, 0x0c, 0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x40, 0x40, 0x12, 0x25, 0x08, 0x49, 0x11, 0xe1, 0x7a, 0x14, 0xae, 0x47, 0x81, 0x24, 0x40, 0xca, 0x3e, 0x05, 0x61, 0x62, 0x63, 0x64, 0x65, 0xca, 0x3e, 0x03, 0x62, 0x63, 0x64, 0xca, 0x3e, 0x03, 0x63, 0x64, 0x65, 0xd2, 0x3e, 0x03, 0x78, 0x79, 0x7a
+            };
+            var s = Serializer.Deserialize<OuterTestStruct>(new MemoryStream(data));
+            Console.WriteLine(s);
+        }
+    }
+}
