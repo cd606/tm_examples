d2ee0819bf0e1f652f891d8947f821175b7d5f3d
cd606 <cd606dc@gmail.com>
2021-08-12 15:21:42 -0500

    add some bcl test

---
+++ bcl_compat_test/CppNoCodeGenClient/main.cpp
@@ -0,0 +1,51 @@
+#include <tm_kit/infra/WithTimeData.hpp>
+#include <tm_kit/infra/RealTimeApp.hpp>
+#include <tm_kit/infra/Environments.hpp>
+#include <tm_kit/infra/TerminationController.hpp>
+
+#include <tm_kit/basic/real_time_clock/ClockComponent.hpp>
+#include <tm_kit/basic/SpdLoggingComponent.hpp>
+
+#include <tm_kit/transport/CrossGuidComponent.hpp>
+#include <tm_kit/transport/MultiTransportRemoteFacilityManagingUtils.hpp>
+#include <tm_kit/transport/bcl_compat/Decimal.hpp>
+#include <tm_kit/transport/bcl_compat/Guid.hpp>
+
+#include "../CppNoCodeGenShare/CppNoCodeGenStruct.hpp"
+
+int main(int argc, char **argv) {
+    using Environment = infra::Environment<
+        infra::CheckTimeComponent<false>
+        , infra::TrivialExitControlComponent
+        , basic::TimeComponentEnhancedWithSpdLogging<basic::real_time_clock::ClockComponent>
+        , transport::CrossGuidComponent
+        , transport::AllNetworkTransportComponents
+    >;
+    using M = infra::RealTimeApp<Environment>;
+    using R = infra::AppRunner<M>;
+
+    Environment env;
+
+    basic::proto_interop::Proto<bcl_compat_test::QueryNoCodeGen<Environment>> q;
+    q->value = boost::multiprecision::cpp_dec_float_100("-0.00000000123");
+    q->description = "cpp_client_test";
+
+    std::cout << q->id << '\n';
+    std::cout << q->value << '\n';
+
+    auto resF = transport::OneShotMultiTransportRemoteFacilityCall<Environment>
+        ::call<
+            basic::proto_interop::Proto<bcl_compat_test::QueryNoCodeGen<Environment>>
+            , basic::proto_interop::Proto<bcl_compat_test::ResultNoCodeGen<Environment>>
+        >(
+            &env
+            //, "redis://127.0.0.1:6379:::bcl_test_queue"
+            , "rabbitmq://127.0.0.1::guest:guest:bcl_test_queue"
+            , std::move(q)
+        );
+    resF.wait();
+    auto res = resF.get();
+
+    std::cout << res->id << '\n';
+    std::cout << res->value << '\n';
+}
---
+++ bcl_compat_test/CppNoCodeGenShare/CppNoCodeGenStruct.hpp
@@ -0,0 +1,30 @@
+#ifndef CPP_NO_PROTO_STRUCT_HPP_
+#define CPP_NO_PROTO_STRUCT_HPP_
+
+#include <tm_kit/basic/SerializationHelperMacros.hpp>
+#include <tm_kit/transport/bcl_compat/BclStructs.hpp>
+
+using namespace dev::cd606::tm;
+
+namespace bcl_compat_test{
+    #define QUERY_FIELDS \
+        ((transport::bcl_compat::BclGuid<Env>, id)) \
+        ((transport::bcl_compat::BclDecimal, value)) \
+        ((std::string, description)) 
+
+    #define RESULT_FIELDS \
+        ((transport::bcl_compat::BclGuid<Env>, id)) \
+        ((transport::bcl_compat::BclDecimal, value)) \
+        ((std::vector<std::string>, messages))
+
+    TM_BASIC_CBOR_CAPABLE_TEMPLATE_STRUCT(((typename, Env)), QueryNoCodeGen, QUERY_FIELDS);
+    TM_BASIC_CBOR_CAPABLE_TEMPLATE_STRUCT(((typename, Env)), ResultNoCodeGen, RESULT_FIELDS);
+}
+
+TM_BASIC_CBOR_CAPABLE_TEMPLATE_STRUCT_SERIALIZE_NO_FIELD_NAMES(((typename, Env)), bcl_compat_test::QueryNoCodeGen, QUERY_FIELDS);
+TM_BASIC_CBOR_CAPABLE_TEMPLATE_STRUCT_SERIALIZE_NO_FIELD_NAMES(((typename, Env)), bcl_compat_test::ResultNoCodeGen, RESULT_FIELDS);
+
+#undef QUERY_FIELDS
+#undef RESULT_FIELDS 
+
+#endif
