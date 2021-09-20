071afb2b30ca0c917e2e2df86c2d4943908a73f7
cd606 <cd606dc@gmail.com>
2021-09-20 16:05:43 -0500

    FTXUI compilation fix

--- simple_demo_chain_version/executables/enable_client/FTXUIMain.cpp
+++ simple_demo_chain_version/executables/enable_client/FTXUIMain.cpp
@@ -1,28 +1,60 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/string.hpp>
-#include <ftxui/component/button.hpp>
-#include <ftxui/component/container.hpp>
+#if defined(_has_include)
+    #if _has_include(<ftxui/component/button.hpp>) && _has_include(<ftxui/component/container.hpp>)
+        #include <ftxui/component/button.hpp>
+        #include <ftxui/component/container.hpp>
+        #define FTXUI_OLD_STYLE_COMPONENT 1
+    #else
+        #include <ftxui/component/component.hpp>
+        #define FTXUI_OLD_STYLE_COMPONENT 0
+    #endif 
+#else 
+    #include <ftxui/component/component.hpp>
+    #define FTXUI_OLD_STYLE_COMPONENT 0
+#endif
 #include <ftxui/component/screen_interactive.hpp>
 
 #include "EnablerGUIDataFlow.hpp"
 
+#if FTXUI_OLD_STYLE_COMPONENT == 1
 class EnablerGUIComponent : public ftxui::Component {
+#else 
+class EnablerGUIComponent : public ftxui::ComponentBase {
+#endif
 private:
     std::function<void(bool &&)> configureFeedFunc_;
     std::function<void()> exitFunc_;
     std::wstring displayText_;
+#if FTXUI_OLD_STYLE_COMPONENT == 1
     ftxui::Button enableBtn_, disableBtn_, exitBtn_;
     ftxui::Container container_;
+#else 
+    ftxui::Component enableBtn_, disableBtn_, exitBtn_;
+    ftxui::Component container_;
+#endif
 public:
     EnablerGUIComponent(std::function<void(bool &&)> const &configureFeedFunc, std::function<void()> const &exitFunc) 
+#if FTXUI_OLD_STYLE_COMPONENT == 1
         : ftxui::Component(), configureFeedFunc_(configureFeedFunc), exitFunc_(exitFunc)
+#else
+        : ftxui::ComponentBase(), configureFeedFunc_(configureFeedFunc), exitFunc_(exitFunc)
+#endif
         , displayText_(L"Unknown")
+#if FTXUI_OLD_STYLE_COMPONENT == 1
         , enableBtn_(L"Enable")
         , disableBtn_(L"Disable")
         , exitBtn_(L"Exit")
         , container_(ftxui::Container::Horizontal())
+#else
+        , enableBtn_()
+        , disableBtn_()
+        , exitBtn_()
+        , container_(ftxui::Container::Horizontal({}))
+#endif
     {
+#if FTXUI_OLD_STYLE_COMPONENT == 1
         enableBtn_.on_click = [this]() {
             configureFeedFunc_(true);
         };
@@ -36,6 +68,21 @@ public:
         container_.Add(&disableBtn_);
         container_.Add(&exitBtn_);
         Add(&container_);
+#else 
+        enableBtn_ = ftxui::Button(L"Enable", [this]() {
+            configureFeedFunc_(true);
+        });
+        disableBtn_ = ftxui::Button(L"Disable", [this]() {
+            configureFeedFunc_(false);
+        });
+        exitBtn_ = ftxui::Button(L"Exit", [this]() {
+            exitFunc_();
+        });
+        container_->Add(enableBtn_);
+        container_->Add(disableBtn_);
+        container_->Add(exitBtn_);
+        Add(container_);
+#endif
     }
     void setEnabled(bool b) {
         if (b) {
@@ -48,9 +95,15 @@ public:
         return ftxui::vbox({
             ftxui::text(displayText_) | ftxui::hcenter
             , ftxui::hbox({
+#if FTXUI_OLD_STYLE_COMPONENT == 1
                 enableBtn_.Render()
                 , disableBtn_.Render()
                 , exitBtn_.Render()
+#else
+                enableBtn_->Render()
+                , disableBtn_->Render()
+                , exitBtn_->Render()
+#endif
             }) | ftxui::hcenter
         });
     }
@@ -80,7 +133,11 @@ int main(int argc, char **argv) {
     //For some reason my build of FTXUI will core dump when calling
     //destructor for this component, therefore I deliberately avoided
     //the destructor call
+#if FTXUI_OLD_STYLE_COMPONENT == 1
     auto *component = new EnablerGUIComponent(configureFeedFunc, exitFeedFunc);
+#else 
+    std::shared_ptr<EnablerGUIComponent> component(new EnablerGUIComponent(configureFeedFunc, exitFeedFunc));
+#endif
 
     auto statusHandler = M::pureExporter<bool>(
         [component,&screen](bool &&data) {
