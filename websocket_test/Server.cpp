761cc121848e449b70fc5f01e52e9d79702b7f88
cd606 <cd606dc@gmail.com>
2021-09-29 09:09:21 -0500

    windows compilation fix

--- websocket_test/Server.cpp
+++ websocket_test/Server.cpp
@@ -12,6 +12,15 @@
 #include <tm_kit/transport/MultiTransportBroadcastPublisherManagingUtils.hpp>
 #include <tm_kit/transport/MultiTransportTouchups.hpp>
 
+#ifdef _MSC_VER
+#define DATA_FIELDS \
+    ((std::chrono::system_clock::time_point, now)) \
+    ((std::string, textData)) \
+    ((uint64_t, intData)) \
+    ((std::vector<double>, doubleData)) \
+    ((TM_BASIC_CBOR_CAPABLE_STRUCT_PROTECT_TYPE(std::variant<int,float>), variantData)) \
+    ((TM_BASIC_CBOR_CAPABLE_STRUCT_PROTECT_TYPE(std::tuple<bool,int>), tupleData))
+#else
 #define DATA_FIELDS \
     ((std::chrono::system_clock::time_point, now)) \
     ((std::string, textData)) \
@@ -19,6 +28,7 @@
     ((std::vector<double>, doubleData)) \
     (((std::variant<int,float>), variantData)) \
     (((std::tuple<bool,int>), tupleData))
+#endif
 
 TM_BASIC_CBOR_CAPABLE_STRUCT(Data, DATA_FIELDS);
 TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE(Data, DATA_FIELDS);
