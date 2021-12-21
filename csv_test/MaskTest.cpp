885d9e34f291fa5e155aa5020e5d233bb1a8a8fd
cd606 <cd606dc@gmail.com>
2021-12-20 22:50:35 -0600

    add concat struct field info

--- csv_test/MaskTest.cpp
+++ csv_test/MaskTest.cpp
@@ -1,6 +1,7 @@
 #include <tm_kit/basic/SerializationHelperMacros.hpp>
 #include <tm_kit/basic/StructFieldInfoWithMaskingFilter.hpp>
 #include <tm_kit/basic/StructFieldInfoBasedCsvUtils.hpp>
+#include <tm_kit/basic/ConcatStructFieldInfo.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -13,8 +14,14 @@ using namespace dev::cd606::tm;
     ((std::string, Description)) \
     ((bool, Check))
 
+#define DATA2_FIELDS \
+    ((double, Data)) \
+    ((double, AnotherData))
+
 TM_BASIC_CBOR_CAPABLE_STRUCT(Data, DATA_FIELDS);
 TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(Data, DATA_FIELDS);
+TM_BASIC_CBOR_CAPABLE_STRUCT(Data2, DATA2_FIELDS);
+TM_BASIC_CBOR_CAPABLE_STRUCT_SERIALIZE_NO_FIELD_NAMES(Data2, DATA2_FIELDS);
 
 inline constexpr bool mask(std::string_view const &s) {
     return (s[0] == 'C');
@@ -56,5 +63,24 @@ int main(int argc, char **argv) {
         std::cout
         , x.begin()
         , x.end()
+    );
+
+    std::cout << "=======================\n";
+    using C = basic::struct_field_info_utils::ConcatStructFields<Data,Data2>;
+    std::vector<C> cs;
+    C c;
+    c.Data::operator=(dataVec[0]);
+    c.Data2::operator=(Data2 {1.2, 3.4});
+    cs.push_back(std::move(c));
+    c.Data::operator=(dataVec[1]);
+    c.Data2::operator=(Data2 {5.6, 7.8});
+    cs.push_back(std::move(c));
+
+    basic::struct_field_info_utils::StructFieldInfoBasedSimpleCsvOutput<
+        C
+    >::writeDataCollection(
+        std::cout
+        , cs.begin()
+        , cs.end()
     );
 }
